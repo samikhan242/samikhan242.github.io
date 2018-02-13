1696a811aff758e74c95ef132d948728d67b5174
samikhan242 <36244430+samikhan242@users.noreply.github.com>
2018-02-13 15:20:22 -0600

    click

--- project/sketch.js
+++ project/sketch.js
@@ -1,23 +1,34 @@
+// p5js template project - replace with project title
+// Dan Schellenberg - replace with your name
+// Feb 2, 2018 - replace with the date
+
+// global variables
+let gear;
+
+// the preload function guarentees that the code inside the function is
+// executed before the rest of the program runs -- helpful for things
+// like loading images (since JS is asynchronous)
+function preload() {
+  gear = loadImage("images/gear.png");
+}
+
+// the setup function will only run once (before the draw loop begins)
+// this is where you want to set up the environment (size of canvas, etc)
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  textSize(45);
+  background(255);
+
+  image(gear, 0, 0);
+
+  stroke(0);
+  line(0, 0, 200, 200);
+
+  fill(0, 255, 0, 100);
   noStroke();
-}
 
-function mouseClicked() {
-  fill(random(225), random(225), random(225), random(225));
-  rect(random(0, width),random(0, height),random(100),random(100));
-}
-
-function keyPressed(){
-  fill(random(225), random(225), random(225), random(225));
-  ellipse(random(0, width),random(0, height),random(100),random(100));
-}
-
-function deviceShaken() {
-  fill(0)
-  text("word",random(0, width) , random(0, height));
+  rect(mouseX, mouseY, 100, 300);
+  ellipse(400, 150, 300, 200);
 }
---
+++ project2/sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(225)
+  draw(mousePressed);
+}
+
+function mousePressed(){
+  if (mouseIsPressed) {
+    noStroke()
+     if (mouseButton === LEFT) {
+       fill(random(225), random(225), random(225), random(225));
+       ellipseMode(CENTER);
+       ellipse(mouseX, mouseY, 50, 50);
+     }
+     if (mouseButton === RIGHT) {
+       fill(random(225), random(225), random(225), random(225));
+       rectMode(CENTER);
+       rect(mouseX, mouseY, 50, 50);
+     }
+     if (mouseButton === CENTER) {
+       fill(random(225), random(225), random(225), random(225));
+       triangle(mouseX-50, mouseY+50, mouseX, mouseY, mouseX+50, mouseY+50);
+     }
+   }
+   print(mouseButton);
+}
