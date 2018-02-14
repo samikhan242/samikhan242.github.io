31946e38bf1bf3bd3f0ffa93da8ec1d4ceb0ad5b
samikhan242 <36244430+samikhan242@users.noreply.github.com>
2018-02-14 15:19:59 -0600

    work

--- project2/sketch.js
+++ project2/sketch.js
@@ -4,27 +4,30 @@ function setup() {
   document.addEventListener("contextmenu", event => event.preventDefault())
 }
 function draw() {
+  // to remove borders;
   noStroke()
+  // mouse Pressed;
   if (mouseIsPressed) {
+    // Left Click for Circle;
    if (mouseButton === LEFT) {
     fill(random(225), random(225), random(225), random(225));
     ellipseMode(CENTER);
     ellipse(mouseX, mouseY, 50, 50);
   }
-
+// Right Click for rect;
   if (mouseButton === RIGHT) {
    fill(random(225), random(225), random(225), random(225));
    rectMode(CENTER);
    rect(mouseX, mouseY, 50, 50);
    }
-
+// Center or wheel click for triangle;
  if (mouseButton === CENTER) {
     fill((random(225)), (random(225)), (random(225)), (random(225)));
     triangle(mouseX-50, mouseY+50, mouseX, mouseY, mouseX+50, mouseY+50);
  }
    print(mouseButton);
  }
-
+// Key Pressed;
   if (keyIsPressed) {
     if (keyCode === ENTER){
       background(random(225));
