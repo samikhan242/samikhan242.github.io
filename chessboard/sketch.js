5fc368cca61ad020b446f7a46ff541e9120f682d
samikhan242 <36244430+samikhan242@users.noreply.github.com>
2018-03-16 15:20:12 -0600

    hj

---
+++ chessboard/sketch.js
@@ -0,0 +1,25 @@
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255,0,0,0);
+  let x = random(0, width);
+  let y = random(0, height);
+
+
+  frameRate (970);
+  rect(0,0,windowWidth,windowHeight);
+  fill(0, 0, 0);
+  ellipse(mouseX, mouseY, 40, 40);
+  stroke(100);
+  fill(255, 255, 255, 51);
+
+
+}
+
+function mouseClicked(){
+  exex
+}
